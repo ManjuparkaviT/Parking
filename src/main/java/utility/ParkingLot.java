b55f19510b6e990976ba9ef542f2a6da703e433b
MANJUPARKAVI T <manjutv1999@gmail.com>
2021-03-24 21:04:12 +0530

    Scenario for throwing exception if the parking slot is not available

--- src/main/java/utility/ParkingLot.java
+++ src/main/java/utility/ParkingLot.java
@@ -1,17 +1,22 @@
 package utility;
 
-public class ParkingLot {
+import javax.naming.LimitExceededException;
 
+public class ParkingLot {
     private int parkingLotCapacity;
 
     public ParkingLot(int parkingLotCapacity) {
         this.parkingLotCapacity = parkingLotCapacity;
     }
 
-    public void parking(Car car) {
-        if(isParkingSlotAvailable()){
+    public void parking(Car car) throws LimitExceededException {
+        if(isParkingSlotAvailable())
+        {
             this.parkingLotCapacity-=1;
             car.setStatus("CarParked");
+        }
+        else {
+            throw new LimitExceededException("No Parking Slot Is Available");
         }
     }
 
--- src/test/java/utility/ParkingLotTest.java
+++ src/test/java/utility/ParkingLotTest.java
@@ -1,12 +1,16 @@
 package utility;
 
 import org.junit.jupiter.api.Test;
+
+import javax.naming.LimitExceededException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParkingLotTest {
 
     @Test
-    public void testIfCarIsParked(){
+    public void testIfCarIsParked() throws LimitExceededException {
         ParkingLot parkingLot = new ParkingLot(20);
         Car car = new Car("AP 405987");
 
@@ -17,7 +21,18 @@ public class ParkingLotTest {
         assertEquals(expectedCarStatus,actualCarStatus);
     }
 
+    @Test
+    public void testThrowsExceptionIfParkingSlotIsNotAvailable() throws LimitExceededException {
+        ParkingLot parkingLot = new ParkingLot(2);
+        Car firstCar = new Car("AP 405987");
+        Car secondCar = new Car("TN 405987");
+        Car thirdCar = new Car("AP 405642");
 
+        parkingLot.parking(firstCar);
+        parkingLot.parking(secondCar);
+        Exception actual = assertThrows(LimitExceededException.class, () -> parkingLot.parking(thirdCar));
 
+        assertEquals("No Parking Slot Is Available",actual.getMessage());
 
+    }
 }
